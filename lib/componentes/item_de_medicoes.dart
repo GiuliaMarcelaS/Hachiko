76244758d84c7bcf122600dfb66f0425eddbcae3
GiuliaMarcelaS <giulia.faria@itec.ufpa.br>
2023-05-24 13:07:14 -0300

    m

---
+++ lib/componentes/item_de_medicoes.dart
@@ -0,0 +1,13 @@
+// import 'package:flutter/src/widgets/framework.dart';
+// import 'package:flutter/src/widgets/placeholder.dart';
+// import 'package:hachiko/componentes/medicoes.dart';
+
+// class ItemDeMedicoes extends StatelessWidget {
+//   final Medicoes medicao;
+//   const ItemDeMedicoes({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
